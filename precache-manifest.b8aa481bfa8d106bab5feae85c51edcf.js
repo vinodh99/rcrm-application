140436fab84c8f178829bf2251abec27580682e1
vinodh tankasala <vinodh2019@gmail.com>
2018-12-11 17:38:32 -0500

    Updates

---
+++ precache-manifest.b8aa481bfa8d106bab5feae85c51edcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5599d603a9a55d54b14d",
+    "url": "/rcrm-application/static/js/runtime~main.5599d603.js"
+  },
+  {
+    "revision": "f0ab787da12d587b194d",
+    "url": "/rcrm-application/static/js/main.f0ab787d.chunk.js"
+  },
+  {
+    "revision": "c87cc1364250b55cdba5",
+    "url": "/rcrm-application/static/js/1.c87cc136.chunk.js"
+  },
+  {
+    "revision": "f0ab787da12d587b194d",
+    "url": "/rcrm-application/static/css/main.983d23ad.chunk.css"
+  },
+  {
+    "revision": "c87cc1364250b55cdba5",
+    "url": "/rcrm-application/static/css/1.c3da8480.chunk.css"
+  },
+  {
+    "revision": "eb1e6a9df0341ceea807183c08405c3f",
+    "url": "/rcrm-application/index.html"
+  }
+];
