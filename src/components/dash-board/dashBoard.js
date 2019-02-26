dc53259d9f2cf923c4b7de54e287a3cf3aa87385
vinodh tankasala <vinodh2019@gmail.com>
2019-02-25 21:43:38 -0500

    resolved university issue

--- src/components/dash-board/dashBoard.js
+++ src/components/dash-board/dashBoard.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import './dashBoard.css';
-import AddEmployee from '../employeeData/addEmployee';
-import SearchEmployee from '../employeeData/searchEmployee';
-import WrappedDemo from '../employeeData/documents';
-import EdDetails from '../employeeData/educationDetails'
-import Home from './home'
-import { Layout, Menu, Icon } from 'antd';
+import React, { Component } from "react";
+import "./dashBoard.css";
+import AddEmployee from "../employeeData/addEmployee";
+import SearchEmployee from "../employeeData/searchEmployee";
+import WrappedDemo from "../employeeData/documents";
+import EdDetails from "../employeeData/educationDetails";
+import Home from "./home";
+import { Layout, Menu, Icon } from "antd";
 
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
 class DashBoard extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -23,53 +22,98 @@ class DashBoard extends Component {
     };
   }
 
-  onButtonClicked = (comp) => {
+  onButtonClicked = comp => {
     switch (comp) {
       case "home":
-        this.setState({ ishome: true, isAddEmp: true, isSearchEmp: false, isdoc: false, isEdDetails: false });
+        this.setState({
+          ishome: true,
+          isAddEmp: true,
+          isSearchEmp: false,
+          isdoc: false,
+          isEdDetails: false
+        });
         break;
       case "add":
-        this.setState({ isAddEmp: true, isSearchEmp: false, isdoc: false, isEdDetails: false, isHome: false });
+        this.setState({
+          isAddEmp: true,
+          isSearchEmp: false,
+          isdoc: false,
+          isEdDetails: false,
+          isHome: false
+        });
         break;
       case "search":
-        this.setState({ isSearchEmp: true, isAddEmp: false, isdoc: false, isEdDetails: false, isHome: false });
+        this.setState({
+          isSearchEmp: true,
+          isAddEmp: false,
+          isdoc: false,
+          isEdDetails: false,
+          isHome: false
+        });
         break;
       case "doc":
-        this.setState({ isdoc: true, isAddEmp: false, isSearchEmp: false, isEdDetails: false, isHome: false });
+        this.setState({
+          isdoc: true,
+          isAddEmp: false,
+          isSearchEmp: false,
+          isEdDetails: false,
+          isHome: false
+        });
         break;
       case "edDetails":
-        this.setState({ isEdDetails: true, isdoc: false, isAddEmp: false, isSearchEmp: false, isHome: false })
+        this.setState({
+          isEdDetails: true,
+          isdoc: false,
+          isAddEmp: false,
+          isSearchEmp: false,
+          isHome: false
+        });
         break;
       default:
         this.setState({ isAddEmp: true, isSearchEmp: false });
     }
-  }
+  };
 
   render() {
-
     const { isAddEmp, isSearchEmp, isdoc, isEdDetails, isHome } = this.state;
-
 
     return (
       // <div>
-      <Layout style={{ minHeight: '100vh' }}>
+      <Layout style={{ minHeight: "100vh" }}>
         <Sider>
           <div className="logo" />
-          <Menu defaultSelectedKeys={['1']} mode="inline">
-            <img src="https://rsrit.com/wp-content/uploads/2017/12/logo_dark.png" alt="image" width="200px" height="70px"></img>
+          <Menu defaultSelectedKeys={["1"]} mode="inline">
+            <img
+              src="https://rsrit.com/wp-content/uploads/2017/12/logo_dark.png"
+              alt="image"
+              width="200px"
+              height="70px"
+            />
 
             <SubMenu
               key="sub1"
-              title={<span><Icon type="user" /><span>Personal Details</span></span>}
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>Personal Details</span>
+                </span>
+              }
             >
-              <Menu.Item key="1" onClick={() => this.onButtonClicked("add")} >Add Applicant</Menu.Item>
-              <Menu.Item key="2" onClick={() => this.onButtonClicked("search")}>Search Applicant</Menu.Item>
+              <Menu.Item key="1" onClick={() => this.onButtonClicked("add")}>
+                Add Applicant
+              </Menu.Item>
+              <Menu.Item key="2" onClick={() => this.onButtonClicked("search")}>
+                Search Applicant
+              </Menu.Item>
             </SubMenu>
             <Menu.Item key="3" onClick={() => this.onButtonClicked("doc")}>
               <Icon type="desktop" />
               <span>Documents</span>
             </Menu.Item>
-            <Menu.Item key="4" onClick={() => this.onButtonClicked("edDetails")}>
+            <Menu.Item
+              key="4"
+              onClick={() => this.onButtonClicked("edDetails")}
+            >
               <Icon type="book" />
               <span>Education</span>
             </Menu.Item>
@@ -88,29 +132,34 @@ class DashBoard extends Component {
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 580 }}>
+          <Header style={{ background: "#fff", padding: 0 }} />
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              background: "#fff",
+              minHeight: 580
+            }}
+          >
             {/* <Link to={{ pathname: "/AddEmployee" }}>Add Employee</Link>
             {"|"}
             <Link to={{ pathname: "/SearchEmployee" }}>Search Employee</Link> */}
-            {(isHome ? (<Home {...this.props} />) : (""))}
-            {(isAddEmp ? (<AddEmployee {...this.props} />) : (""))}
-            {(isSearchEmp ? (<SearchEmployee  {...this.props} />) : (""))}
-            {(isdoc ? (<WrappedDemo  {...this.props} />) : (""))}
-            {(isEdDetails ? (<EdDetails {...this.props} />) : (""))}
+            {isHome ? <Home {...this.props} /> : ""}
+            {isAddEmp ? <AddEmployee {...this.props} /> : ""}
+            {isSearchEmp ? <SearchEmployee {...this.props} /> : ""}
+            {isdoc ? <WrappedDemo {...this.props} /> : ""}
+            {isEdDetails ? <EdDetails {...this.props} /> : ""}
+            {/* {activeTabId === 'EdDetails' && <EdDetails {...this.props} />} */}
           </Content>
-          <Footer style={{ textAlign: 'center' }}>
+          <Footer style={{ textAlign: "center" }}>
             {/* Ant Design ©2018 Created by Ant UED */}
           </Footer>
         </Layout>
       </Layout>
 
       // {/* </div> */}
-
     );
   }
 }
-
-
 
 export default DashBoard;
--- src/components/employeeData/educationDetails.js
+++ src/components/employeeData/educationDetails.js
@@ -1,157 +1,170 @@
-
-import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import {
-    Form, Row, Col, Input, Button, Icon, Card
-} from 'antd';
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { Form, Row, Col, Input, Button, Icon, Card } from "antd";
 class EdDetails extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            expand: false,
-            universityList: [this.university()],
-            count: 0
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      expand: false,
+      universityList: [this.university],
+      count: 0
+    };
+  }
 
-    // To generate mock Form.Item
-    getFields() {
-        // const count = this.state.expand ? 10 : 6;
-        const { getFieldDecorator } = this.props.form;
+  // To generate mock Form.Item
+  getFields() {
+    // const count = this.state.expand ? 10 : 6;
+    const { getFieldDecorator } = this.props.form;
 
-        const formItemLayout = {
-            labelCol: { span: 6 },
-            wrapperCol: { span: 14 }
-        };
-        const children = [];
-        const fields = [['School/University', 'Name'], ['Degree', 'Bachelors/Masters'], ['Years Completed', 'years'], ['Major Study', 'Specialization'], ['Minor Study', 'field'], ['GPA', ''], ['Country', ''], ['State', ''], ['City', '']];
-        for (let i = 0; i < fields.length; i++) {
-            if (i === 0 || i === 1 || i === 2 || i === 3) {
-                children.push(
-                    <Col span={12} key={i}>
-                        <Form.Item {...formItemLayout} label={`${fields[i][0]}`}>
-                            {getFieldDecorator(`field-${i}`, {
-                                rules: [{
-                                    required: true,
-                                    message: 'Input something!',
-                                }],
-                            })(
-                                <Input placeholder={fields[i][1]} />
-                            )}
-                        </Form.Item>
-                    </Col>
-                );
-            }
-            else {
-                children.push(
-                    <Col span={12} key={i}>
-                        <Form.Item {...formItemLayout} label={`${fields[i][0]}`}>
-                            {getFieldDecorator(`field-${i}`, {
-                                rules: [{
-                                    required: false,
-                                    // message: 'Input something!',
-                                }],
-                            })(
-                                <Input placeholder={fields[i][1]} />
-                            )}
-                        </Form.Item>
-                    </Col>
-                );
-            }
-        }
-        return children;
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 }
+    };
+    const children = [];
+    const fields = [
+      ["School/University", "Name"],
+      ["Degree", "Bachelors/Masters"],
+      ["Years Completed", "years"],
+      ["Major Study", "Specialization"],
+      ["Minor Study", "field"],
+      ["GPA", ""],
+      ["Country", ""],
+      ["State", ""],
+      ["City", ""]
+    ];
+    for (let i = 0; i < fields.length; i++) {
+      if (i === 0 || i === 1 || i === 2 || i === 3) {
+        children.push(
+          <Col span={12} key={i}>
+            <Form.Item {...formItemLayout} label={`${fields[i][0]}`}>
+              {getFieldDecorator(`field-${i}`, {
+                rules: [
+                  {
+                    required: true,
+                    message: "Input something!"
+                  }
+                ]
+              })(<Input placeholder={fields[i][1]} />)}
+            </Form.Item>
+          </Col>
+        );
+      } else {
+        children.push(
+          <Col span={12} key={i}>
+            <Form.Item {...formItemLayout} label={`${fields[i][0]}`}>
+              {getFieldDecorator(`field-${i}`, {
+                rules: [
+                  {
+                    required: false
+                    // message: 'Input something!',
+                  }
+                ]
+              })(<Input placeholder={fields[i][1]} />)}
+            </Form.Item>
+          </Col>
+        );
+      }
     }
+    return children;
+  }
 
-    handleSearch = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            console.log('Received values of form: ', values);
-        });
-    }
-    componentDidMount() {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-    handleReset = () => {
-        this.props.form.resetFields();
-    }
+  handleSearch = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      console.log("Received values of form: ", values);
+    });
+  };
+  componentDidMount() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  handleReset = () => {
+    this.props.form.resetFields();
+  };
 
-    toggle = () => {
-        const { expand } = this.state;
-        this.setState({ expand: !expand });
-    }
-    changeList = () => {
-        console.log(this.state.count)
-        this.setState({
-            count: this.state.count + 1,
-            universityList: [...this.state.universityList, this.university(this.state.count)],
-        })
-        console.log(this.state.count)
-        // val++;
+  toggle = () => {
+    const { expand } = this.state;
+    this.setState({ expand: !expand });
+  };
+  changeList = () => {
+    console.log(this.state.count);
+    this.setState({
+      count: this.state.count + 1,
+      universityList: [...this.state.universityList, this.university]
+    });
+    console.log(this.state.count);
+    // val++;
 
-
-        // this.setState({ universityList: [this.state.universityList[0].splice(1)] })
-    }
-    deleteList = (e) => {
-        console.log("target value", e.target.value)
-        let array = [...this.state.universityList]; // make a separate copy of the array
-        // let index = array.indexOf(e.target.value)
-        // if (index !== -1) {
-        array.splice(e.target.value);
-        this.setState({ universityList: array });
-        // }
-    }
-    university = (value) => {
-        // const { i } = this.state.i;
-        return (
-            <div style={{ paddingBottom: 40 }}>
-                <Card
-                    type="inner"
-                    title="University / School"
-                    extra={<Button value={value} onClick={(e) => this.deleteList(e)}>Delete</Button>}
-                >
-                    <Form
-                        className="ant-advanced-search-form"
-                        onSubmit={this.handleSearch}
-                    >
-                        <Row gutter={24}>{this.getFields()}</Row>
-                        <Row>
-                            <Col span={24} style={{ textAlign: 'right' }}>
-                                <Button type="primary" htmlType="submit">Submit</Button>
-                                <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
-                                    Clear
-                            </Button>
-                                {/* <a style={{ marginLeft: 8, fontSize: 12 }} onClick={this.toggle}>
+    // this.setState({ universityList: [this.state.universityList[0].splice(1)] })
+  };
+  deleteList = e => {
+    console.log("target value", e.target.value);
+    let array = [...this.state.universityList]; // make a separate copy of the array
+    // let index = array.indexOf(e.target.value)
+    // if (index !== -1) {
+    array.splice(e.target.value);
+    this.setState({ universityList: array });
+    // }
+  };
+  university = value => {
+    // const { i } = this.state.i;
+    return (
+      <div style={{ paddingBottom: 40 }}>
+        <Card
+          type="inner"
+          title="University / School"
+          extra={
+            <Button value={value} onClick={e => this.deleteList(e)}>
+              Delete
+            </Button>
+          }
+        >
+          <Form
+            className="ant-advanced-search-form"
+            onSubmit={this.handleSearch}
+          >
+            <Row gutter={24}>{this.getFields()}</Row>
+            <Row>
+              <Col span={24} style={{ textAlign: "right" }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+                <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                  Clear
+                </Button>
+                {/* <a style={{ marginLeft: 8, fontSize: 12 }} onClick={this.toggle}>
                                 Collapse <Icon type={this.state.expand ? 'up' : 'down'} />
                             </a> */}
-                            </Col>
-                        </Row>
-                    </Form>
-                </Card>
-            </div>
-        )
-    }
-    render() {
-
-        return (
-            < div >
-                {this.university()}
-
-                <div>
-                    {this.state.universityList.map((university, index) => (
-                        <div key={index}>{university}</div>
-                    ))}
-                </div>
-                <Button value={this.state.count} style={{ marginTop: 8 }} onClick={e => this.changeList(e)}>
-                    <Icon type="plus" />Add another school/University
-                </Button>
-            </div >
-        );
-    }
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </div>
+    );
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.universityList.map((university, index) => (
+            <div key={index}>{university(index + 1)}</div>
+            // <University />
+          ))}
+        </div>
+        <Button
+          value={this.state.count}
+          style={{ marginTop: 8 }}
+          onClick={e => this.changeList(e)}
+        >
+          <Icon type="plus" />
+          Add another school/University
+        </Button>
+      </div>
+    );
+  }
 }
 
-const WrappedEdDetails = Form.create({ name: 'advanced_search' })(EdDetails);
-
+const WrappedEdDetails = Form.create({ name: "advanced_search" })(EdDetails);
 
 export default WrappedEdDetails;
